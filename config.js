78cbafa1cc6d8e533cd10a679b83a134c0ef8d45
nemo <tanhuy5543@gmail.com>
2021-12-21 21:46:54 +0700

    a

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Chọn đi :>',
+    desc: 'design by Huy ',
+    btnYes: 'Vẫn cứ là thích anh í',
+    btnNo: 'Không, Anh trai à ',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay ship cho ly trà sữa nhó :3.',
+    btnAccept: 'Okiiiii lunnn',
+} 
